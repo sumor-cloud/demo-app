bb56693ff343289c309b491e2e46c8a67783eacc
Oh? Now! <dennis@sumor.com>
2024-04-02 14:06:15 +0800

    feature 1

---
+++ app.js
@@ -0,0 +1,15 @@
+import createServer from "@sumor/ssl-server";
+
+const app = createServer();
+
+const versionInfo = {
+    feature1: "ABC"
+};
+
+app.all("/", (req, res) => {
+    res.json(versionInfo);
+});
+
+await app.listen();
+
+console.log(`server running on https://localhost`);
